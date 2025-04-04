f520021a85e1332964f11ad7106f1683e47d70ff
davidkochanski <kochanskidavid9@gmail.com>
2025-04-04 13:45:52 -0400

    Ready to present

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-
+import * as THREE from "three";
+import "./style.css"
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import { GITHUB, LINKEDIN } from "./urls";
 
 // ##############################################
 // Initial Setup
 // ##############################################
 
+const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.up.set(0,0,1);
+camera.position.y = 200;
 
+const rootElement = document.getElementById("space");
 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(rootElement.clientWidth, rootElement.clientHeight);
+rootElement.appendChild(renderer.domElement);
 
 
 // ##############################################
@@ -17,27 +28,24 @@
 
 
 
-
-
-
-
-
-
-
-
-
 // ##############################################
 // Load Textures
 // ##############################################
 
+// const loader = new THREE.TextureLoader();
 
+// const planetMap = loader.load("planet_normal.png");
 
+// const galaxyMap = loader.load("galaxy.png")
 
+// const moonTextureMap = loader.load("moon_texture.jpg")
+// const moonNormalMap = loader.load("moon_normal.png")
 
+// const linkedinCube = loader.load("linkedin.png")
+// const githubCube = loader.load("github.png")
 
-
-
-
+// const flagFrontMap = loader.load("welcome_flag_front.png")
+// const flagBackMap = loader.load("welcome_flag_back.png")
 
 
 // ##############################################
@@ -46,38 +54,9 @@
 
 
 
-
-
-
-
-
-
-
-
-
 // ##############################################
 // Lighting
 // ##############################################
-
-
-
-
-
-
-
-
-
-
-
-// ##############################################
-// Skybox Background
-// ##############################################
-
-
-
-
-
-
 
 
 
@@ -88,12 +67,9 @@
 
 
 
-
-
-
-
-
-
+// ##############################################
+// Skybox Background
+// ##############################################
 
 
 // ##############################################
@@ -104,130 +80,44 @@
 
 
 
-
-
-
-
-
-
-
-
 // ##############################################
 // Define items on the planet
 // ##############################################
 
+const itemBlueprints = [
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+]
 
 
 // ##############################################
 // Render Items on the planet
 // ##############################################
 
+const itemMeshes = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+itemBlueprints.forEach((item) => {
+    
+});
 
 
 // ##############################################
 // Raycasting
 // ##############################################
 
+// function castRay(event) {
+//     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+//     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ##############################################
-// Highlight effect on hover
-// ##############################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ##############################################
-// Event triggering on click
-// ##############################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     raycaster.setFromCamera(mouse, camera);
+//     return raycaster.intersectObjects(scene.children, true);
+// }
 
 // ##############################################
 // Main Loop & Basic Resize Handling
 // ##############################################
 
 function animate() {
-    
+
 }
 
 animate();
--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,5 @@
 export const LINKEDIN = "https://www.linkedin.com/in/davidkochanski/";
-export const GITHUB = "https://www.linkedin.com/in/davidkochanski/";
+export const GITHUB = "https://github.com/davidkochanski";
 
 export const PROJECT1 = "https://github.com/davidkochanski/crazy-chess"
 export const PROJECT2 = "https://github.com/davidkochanski/pokeguesser-react"
